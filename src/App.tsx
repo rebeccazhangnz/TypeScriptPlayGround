ac391bda4562cc2e09427bdedec89f05697c9fc5
Rebecca Zhang <rebecca.zhang@spark.co.nz>
2019-11-04 14:16:33 +1300

    Added TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ReactTypeScript from './components/ReactTypeScript'
+
+function App() {
+  return (
+    <div className="App">
+      <ReactTypeScript/>
+    </div>
+  );
+}
+
+export default App;
